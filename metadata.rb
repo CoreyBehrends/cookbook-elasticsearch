f067cad532be42cd483873174a7211d633574160
CoreyBehrends <cbehrends@beapro.com>
2014-12-30 13:52:46 -0600

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "ian.ehlert@sportngin.com"
 license          "Apache"
 description      "Installs elasticsearch"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
-version          "0.1.0"
+version          "0.1.4"
 
 depends "java"
